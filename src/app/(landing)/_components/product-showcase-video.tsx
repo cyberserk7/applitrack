9ac81736c7b7ea15ca7fffea6e46a69a4e47b386
cyberserk7 <deynilabjo@gmail.com>
2024-08-09 12:54:26 +0530

    compressed the videos

--- src/app/(landing)/_components/product-showcase-video.tsx
+++ src/app/(landing)/_components/product-showcase-video.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export const ProductShowcaseVideo = () => {
   return (
     <video
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -5,7 +5,7 @@ import { ProductShowcaseVideo } from "./_components/product-showcase-video";
 
 export default function Home() {
   return (
-    <div className="min-h-full flex flex-col justify-center pt-20 md:pt-32 pb-10 lg:items-center gap-10 md:gap-28 overflow-y-auto">
+    <div className="min-h-full flex flex-col justify-center pt-20 md:pt-32 pb-10 lg:items-center gap-10 md:gap-20 overflow-y-auto">
       <div className="w-full flex items-center justify-center">
         <CallToAction />
       </div>
