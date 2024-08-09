c4aacc778172e233ced05cd503f9750918f375be
cyberserk7 <deynilabjo@gmail.com>
2024-08-09 14:44:07 +0530

    changed from mp4 to mkv

--- src/app/(landing)/_components/product-showcase-video.tsx
+++ src/app/(landing)/_components/product-showcase-video.tsx
@@ -1,13 +1,10 @@
 "use client";
 
-import { Skeleton } from "@/components/ui/skeleton";
-import { Suspense } from "react";
-
 export const ProductShowcaseVideo = () => {
   return (
     <>
       <video
-        src="/videos/product-showcase.mp4"
+        src="/videos/product-showcase.mkv"
         autoPlay
         muted
         loop
