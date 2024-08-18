f6e3fd62f95569ff006012218e1e223c0dc529ab
cyberserk7 <deynilabjo@gmail.com>
2024-08-18 16:29:11 +0530

    (fix)fetch-count: RevalidatePath added in get app count route

--- src/app/api/get-application-count/route.ts
+++ src/app/api/get-application-count/route.ts
@@ -1,10 +1,14 @@
 import dbConnect from "@/lib/db-connect";
 import ApplicationCountModel from "@/models/ApplicationCount";
+import { revalidatePath } from "next/cache";
 
 export async function GET(req: Request) {
     await dbConnect();
     try {
         const res = await ApplicationCountModel.find();
+
+        revalidatePath("/");
+
         return Response.json({  count: res[0].count }, {
             status: 200,
         })
